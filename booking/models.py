4dc03d6732d6f412be84b25755c7eb35ac70e526
salms358 <sa083850@gmail.com>
2023-07-21 14:12:44 +0000

    creating the UserProfile and Bookings classes

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+STATUS = ((0, "Booking Pending"), (1, "Booking Accepted"))
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    phone_number = models.CharField(max_length=11)
+    client_email = models.CharField(max_length=50)
+
+
+class Bookings(models.Model):
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
+    date = models.DateField()
+    customer_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=11)
+    name = models.CharField(max_length=25)
+    capacity = models.IntegerField
